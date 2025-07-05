3b579dded31239714046839d0d94eb30a32711c4
sahil-kk <oprsahilkk@gmail.com>
2025-07-05 15:36:33 +0530

    popup mobile fix

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -2,7 +2,7 @@ export default function Youtube() {
     return (
         <section id="youtube" className="bg-white py-6 px-6">
             <div
-                className=" max-w-7xl mx-auto bg-slate-900 min-h-[350px] rounded-3xl p-8 md:p-16 flex flex-col md:flex-row md:text-2xl justify-between items-center text-white shadow-sm">
+                className=" max-w-6xl mx-auto sm:mx-5 bg-slate-900 min-h-[350px] rounded-3xl p-8 md:p-16 flex flex-col md:flex-row md:text-2xl justify-between items-center text-white shadow-sm">
                 <h2 className="text-3xl sm:text-xl md:text-3xl font-bold leading-normal mb-2 md:mb-0 text-center md:text-left">
                     Learn with fun. Check out<br />
                     <span className="text-orange-500">Crafted</span> YouTube Channel
@@ -11,7 +11,7 @@ export default function Youtube() {
                     href="https://www.youtube.com/@yourchannel" // Replace with actual channel link
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="bg-orange-500 hover:bg-orange-400 text-white px-11 py-[25px] rounded-[23px] font-semibold transition"
+                    className="bg-orange-500 hover:bg-orange-400 text-white px-11 py-[24px] rounded-[23px] font-semibold transition"
                 >
                     <span className="text-white text-[23px]">YouTube</span>
                 </a>
