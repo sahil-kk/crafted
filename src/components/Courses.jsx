b05d33639e19b0c8dd10ae86f9f0228aa77c6b4e
sahil-kk <oprsahilkk@gmail.com>
2025-08-08 18:24:03 +0530

    dheeraj to maths

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -91,11 +91,12 @@ const CourseCard = ({ course }) => (
         <div className="p-4 text-center  font-baloo">
            <div className="font-semibold"> <h3 className="text-3xl font-semibold text-gray-800">{course.title}</h3></div>
             <p className="text-md text-gray-600 mt-2">{course.desc}</p>
-            <Link to={course.path}>
-                <button className="mt-4 px-4 py-2 border-2 border-black text-black bg-transparent text-lg rounded-xl hover:scale-105 hover:!border-white hover:!bg-orange-500 hover:!text-white transition duration-300">
-                    View Details
-                </button>
-            </Link>
+            <button
+                onClick={(e) => e.preventDefault()} // Keeps it clickable but does nothing
+                className="mt-4 px-4 py-2 border-2 border-black text-black bg-transparent text-lg rounded-xl hover:scale-105 hover:!border-white hover:!bg-orange-500 hover:!text-white transition duration-300"
+            >
+                View Details
+            </button>
         </div>
     </div>
 );
